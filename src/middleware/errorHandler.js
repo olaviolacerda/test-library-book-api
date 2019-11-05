a09a463730c75f659c2234f735222aa047146e83
olaviolacerda <olavio.lacerda@hotmail.com>
2019-11-04 23:15:56 -0300

    added readme

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,12 +1,8 @@
 module.exports = (err, res) => {
-  if (res.headersSent) {
-    const { statusCode, message, code } = err;
-    res.status(statusCode).json({
-      timestamp: new Date().getTime(),
-      code,
-      message,
-    });
-  }
-
-  res.status(500).json({ code: 'INTERNAL_SERVER_ERROR' });
+  const { statusCode, message, code } = err;
+  res.status(statusCode).json({
+    timestamp: new Date().getTime(),
+    code,
+    message,
+  });
 };
